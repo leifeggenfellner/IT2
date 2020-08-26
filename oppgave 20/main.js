2a320ad5f85005c1c6dd1831ad42236c678ca71b
Leizzz69 <54780018+Leizzz69@users.noreply.github.com>
2020-08-26 09:35:18 +0200

    Update main.js

--- oppgave 20/main.js
+++ oppgave 20/main.js
@@ -1,11 +1,11 @@
 const board = (size) => {
   let output = "";
   for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      output += i % 2 === j % 2 ? " " : "*";
+    output += i % 2 === 0 ? "* " : " ";
+    for (let j = 0; j < size * 2; j++) {
+      output += j % 2 === 0 ? "* " : "";
     }
-
-    i !== size - 1 ? (output += "\n") : "";
+    output += i !== size - 1 ? "\n" : "";
   }
   return output;
 };
