18ba3a57fd5420a72e5035da3ac55a6fa31bae34
Leizzz69 <54780018+Leizzz69@users.noreply.github.com>
2020-08-27 22:01:04 +0200

    Add files via upload

---
+++ oppgave 24/main.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("inputField");
+const btn = document.getElementById("submit");
+const sum_display = document.getElementById("sum");
+const array_len = document.getElementById("array_len");
+const average = document.getElementById("average");
+
+let num;
+let sum;
+let len;
+let nums = [];
+
+
+function addNum() {
+    if (input.value != "") {
+        num = parseFloat(input.value);
+        nums.push(num);
+    }
+}
+
+btn.onclick = function () {
+    addNum();
+    sum = nums.reduce((x, y) => x + y, 0);
+    len = nums.length;
+    input.value = "";
+    sum_display.innerHTML = sum;
+    array_len.innerHTML = len;
+    average.innerHTML = sum / len;
+};
