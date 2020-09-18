a2916e8298cb030d1b606fb482ab6147ac8feffd
Leizzz69 <leif.eggenfellner@gmail.com>
2020-09-18 19:03:27 +0200

    deleted pong

--- pong/js/game.js
+++
@@ -1,5 +0,0 @@
-function collide(player, ball) {
-    if (player.left < ball.right && playeer.right > ball.left && player.top < ball.bottom && player.bottom > ball.top) {
-        ball.dy = -ball.dy
-    }
-}
