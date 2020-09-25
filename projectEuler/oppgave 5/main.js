bafa943c074a2d31c60c6e67268470ed9bde0364
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-09-25 16:56:12 +0200

    file upload

---
+++ projectEuler/oppgave 5/main.js
@@ -0,0 +1,31 @@
+// Bad Original Code
+/**
+ * 
+ * @param {number} n 
+ */
+
+function smallestNum(n) {
+    let inc = 2;
+    let step = 2;
+    let smallestNum = 2;
+
+    while (smallestNum <= Number.MAX_SAFE_INTEGER) {
+        for (let i = 2; i <= n; i++) {
+            const divisible = smallestNum % i === 0;
+            if (!divisible) {
+                break;
+            }
+            if (i === inc) {
+                step = smallestNum;
+                inc++;
+            }
+            if (i === n) {
+                return smallestNum;
+            }
+        }
+        smallestNum += step;
+    }
+    return smallestNum;
+}
+
+console.log(smallestNum(20));
