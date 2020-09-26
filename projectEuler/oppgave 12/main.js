32a551296ff4a0fa926c9ca0fd49eb31c5e991d4
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-09-26 16:12:49 +0200

    file upload

---
+++ projectEuler/oppgave 12/main.js
@@ -0,0 +1,27 @@
+function triangleDivs(n) {
+    function divisors(n) {
+        let numFactors = 0;
+        for (let i = 1; i < Math.ceil(Math.sqrt(n)) + 1; i++) {
+            if (n % i === 0) {
+                numFactors += 2;
+            }
+            if (i * i === n) {
+                numFactors--;
+            }
+        }
+        return numFactors;
+    }
+
+    let count;
+    for (let n = 1; n < Number.MAX_SAFE_INTEGER; n++) {
+        let tNum = (n * (n + 1) / 2);
+        if (n % 2 === 0) {
+            count = divisors(n / 2) * divisors((n + 1) / 2);
+        } else {
+            count = divisors(n) * divisors((n + 1) / 2);
+        }
+        if (count >= 500) {
+            return tNum;
+        }
+    }
+}
