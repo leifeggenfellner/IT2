16a0958de6ae12b546910e4b16bd4e653156d08e
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-10-31 18:11:51 +0100

    file upload

---
+++ projectEuler/problem 25/main.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @param {number} n BigInt 
+ */
+
+const fib = (n) => {
+    let arr = new Array(n + 1n);
+    arr[1] = 1n;
+    arr[2] = 1n;
+    for (let i = 3n; i < n + 1n; i++){
+        arr[i] = arr[i - 1n] + arr[i - 2n];
+    }
+
+    return arr[n];
+}
+
+/**
+ * 
+ * @param {Number} i BigInt
+ */
+
+const numLength = (i) => {
+    while (i < Number.MAX_SAFE_INTEGER) {
+        if (fib(i).toString().length >= 1000) return parseInt(i);
+        i++;
+    }
+}
