86dd3981860b757d11463f5c62db151c88e97d21
Leif Eggenfellner <leif.eggenfellner@gmail.com>
2020-11-03 23:05:26 +0100

    file upload

--- projectEuler/problem 31/main.js
+++ projectEuler/problem 31/main.js
@@ -1,11 +1,7 @@
-/**
- * @author Leif Eggenfellner
- */
-
-let coins = [1, 2, 5, 10, 20, 50, 100, 200];
 
 /**
  * 
+ * @author Leif Eggenfellner
  * @param {Number} t Target
  * @param {Array.<Number>} c Coins
  * @returns Returns how many different ways can £2 be made using any number of coins
