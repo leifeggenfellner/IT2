97cb141008e8eb1142babd9cf3f45a1e2679edc4
Leizzz69 <leif.eggenfellner@gmail.com>
2020-12-21 12:54:22 +0100

    file upload

--- tests/høst 2020/oppgave 2/main.js
+++ tests/høst 2020/oppgave 2/main.js
@@ -1,3 +1,32 @@
+class Receipt {
+    constructor(veiachle, price) {
+        this.veiachle = veiachle;
+        this.price = price;
+        this.date = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`;
+        this.time = `${new Date().getHours()}.${new Date().getMinutes().toString().padStart(2, 0)}`;
+    }
+}
+
+const total = arr => {
+    let personbiler = 0;
+    let motorsykler = 0;
+    let lastebiler = 0;
+    let total = 0;
+
+    for (e in arr) {
+        if (arr[e].veiachle === "Personbil") personbiler++;
+        else if (arr[e].veiachle === "Motorsykkel") motorsykler++;
+        else if (arr[e].veiachle === "Lastebil") lastebiler++;
+        total += arr[e].price;
+    }
+
+    return `Personbiler: ${personbiler}\nMotorsykler: ${motorsykler}\nLastebiler: ${lastebiler}\n\nTotalt beløp: ${total}`;
+}
+
+const printReciept = arr => {
+    return `Ferjeselskapet AS\n${arr[arr.length - 1].date} ${arr[arr.length - 1].time}\n#${arr.length.toString().padStart(7, 0)}\n\n\n1 x ${arr[arr.length - 1].veiachle} ${arr[arr.length - 1].price},-\nInkl. mva. (8%): ${((arr[arr.length - 1].price) / 108 * 8).toFixed(2)}`;
+}
+
 const output = document.getElementById("output");
 const personbil = document.getElementById("personbil");
 const motorsykkel = document.getElementById("motorsykkel");
@@ -7,61 +36,22 @@ const daily = document.getElementById("dagsoppgjør");
 const sales = new Array();
 
 personbil.onclick = () => {
-    sales.push({
-        veiachle: "Personbil",
-        price: 150,
-        date: `${new Date().getDate()}.${new Date().getMonth()}.${new Date().getFullYear()}`,
-        time: `${new Date().getHours()}.${new Date().getMinutes().toString().padStart(2, 0)}`
-    });
-
-    output.innerText = `Ferjeselskapet AS\n${sales[sales.length - 1].date} 
-    ${sales[sales.length - 1].time}\n#${sales.length.toString().padStart(7, 0)}
-    \n\n1 x ${sales[sales.length - 1].veiachle} ${sales[sales.length - 1].price},-\n
-    Inkl. mva. (8%): ${((sales[sales.length - 1].price) / 108 * 8).toFixed(2)}`;
+    sales.push(new Receipt("Personbil", 150));
+    output.innerText = printReciept(sales);
 }
 
 motorsykkel.onclick = () => {
-    sales.push({
-        veiachle: "Motorsykkel",
-        price: 100,
-        date: `${new Date().getDate()}.${new Date().getMonth()}.${new Date().getFullYear()}`,
-        time: `${new Date().getHours()}.${new Date().getMinutes().toString().padStart(2, 0)}`
-    });
+    sales.push(new Receipt("Motorsykkel", 100));
+    output.innerText = printReciept(sales);
 
-    output.innerText = `Ferjeselskapet AS\n${sales[sales.length - 1].date} 
-    ${sales[sales.length - 1].time}\n#${sales.length.toString().padStart(7, 0)}
-    \n\n1 x ${sales[sales.length - 1].veiachle} ${sales[sales.length - 1].price},-\n
-    Inkl. mva. (8%): ${((sales[sales.length - 1].price) / 108 * 8).toFixed(2)}`;
 }
 
 lastebil.onclick = () => {
-    sales.push({
-        veiachle: "Lastebil",
-        price: 375,
-        date: `${new Date().getDate()}.${new Date().getMonth()}.${new Date().getFullYear()}`,
-        time: `${new Date().getHours()}.${new Date().getMinutes().toString().padStart(2, 0)}`
-    });
-
-    output.innerText = `Ferjeselskapet AS\n${sales[sales.length - 1].date} 
-    ${sales[sales.length - 1].time}\n#${sales.length.toString().padStart(7, 0)}
-    \n\n1 x ${sales[sales.length - 1].veiachle} ${sales[sales.length - 1].price},-\n
-    Inkl. mva. (8%): ${((sales[sales.length - 1].price) / 108 * 8).toFixed(2)}`;
+    sales.push(new Receipt("Lastebil", 375));
+    output.innerText = printReciept(sales);
 }
 
 daily.onclick = () => {
-    let personbiler = 0;
-    let motorsykler = 0;
-    let lastebiler = 0;
-    let total = 0;
-
-    for (e in sales) {
-        if (sales[e].veiachle === "Personbil") personbiler++;
-        else if (sales[e].veiachle === "Motorsykkel") motorsykler++;
-        else if (sales[e].veiachle === "Lastebil") lastebiler++;
-        total += sales[e].price;
-    }
-
-    output.innerText = `Personbiler: ${personbiler}\nMotorsykler: 
-    ${motorsykler}\nLastebiler: ${lastebiler}\n\nTotalt beløp: ${total}`;
+    output.innerText = total(sales);
 }
 
